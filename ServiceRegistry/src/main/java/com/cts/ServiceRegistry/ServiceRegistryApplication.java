8f8947e02c0b84ff4158cb10ae9e697b9db1d4d2
mukundtheamateur <mukundshukla000@gmail.com>
2024-06-25 16:43:08 +0530

    new

--- ServiceRegistry/src/main/java/com/cts/ServiceRegistry/ServiceRegistryApplication.java
+++ ServiceRegistry/src/main/java/com/cts/ServiceRegistry/ServiceRegistryApplication.java
@@ -3,12 +3,10 @@ package com.cts.ServiceRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @EnableEurekaServer
 @SpringBootApplication
-@ComponentScan(basePackages = "com.cts.controller")
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
--- UserService/src/main/java/com/cts/user/UserServiceApplication.java
+++ UserService/src/main/java/com/cts/user/UserServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @Slf4j
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
